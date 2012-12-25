28d061fca71837aaece38b1d58e28976b2b7d185
Tiezhen WANG <jwzxgo@gmail.com>
2012-12-25 22:17:34 +0100

    @requireLogin

--- main.py
+++ main.py
@@ -2,25 +2,39 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
+class AuthenticatedPage(webapp.RequestHandler):
+    pass
+
+def requireLogin(f):
+    def wrapper(self, *args, **kwargs):
+        # TODO: check how users are used?
         user = users.get_current_user()
 
         if user:
             self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, ' + user.nickname())
+            f(self, *args, **kwargs)
         else:
             self.redirect(users.create_login_url(self.request.uri))
-
-class NewArticlePage(webapp.RequestHandler):
-    def post(self):
         pass
+    return wrapper
 
-class WordsPage(webapp.RequestHandler):
+class MainPage(AuthenticatedPage):
+    @requireLogin
     def get(self):
         pass
 
-class ArticlesPage(webapp.RequestHandler):
+class NewArticlePage(AuthenticatedPage):
+    @requireLogin
+    def getWord(self):
+        pass
+
+class WordsPage(AuthenticatedPage):
+    @requireLogin
+    def get(self):
+        pass
+
+class ArticlesPage(AuthenticatedPage):
+    @requireLogin
     def get(self):
         pass
 
