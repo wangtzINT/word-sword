4686580f8c4ebc1395739c5d3874b06a5c39cfcf
Tiezhen WANG <jwzxgo@gmail.com>
2012-12-25 21:51:04 +0100

    Basic structure of main.py

--- main.py
+++ main.py
@@ -12,8 +12,23 @@ class MainPage(webapp.RequestHandler):
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
+class NewArticlePage(webapp.RequestHandler):
+    def post(self):
+	pass
+
+class WordsPage(webapp.RequestHandler):
+    def get(self):
+	pass
+
+class ArticlesPage(webapp.RequestHandler):
+    def get(self):
+        pass
+
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [('/', MainPage),
+				      ('/new', NewArticlePage),
+				      ('/words', WordsPage),
+				      ('/articles', ArticlesPage)],
                                      debug=True)
 
 def main():
