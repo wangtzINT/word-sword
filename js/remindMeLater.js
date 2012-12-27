9c7466b0738790e63e57b6bd069d21810ed75b81
Tiezhen WANG <jwzxgo@gmail.com>
2012-12-27 15:53:33 +0100

    line cross to remind word later

--- js/remindMeLater.js
+++ js/remindMeLater.js
@@ -1,2 +1,14 @@
 $(document).ready(function(){
+    var options = {title: "click on the word to remind it later"};
+    $(".wordName").tooltip(options);
+    $(".wordElement").click(function(){
+        var element = $(this)
+        var style = element.css("text-decoration");
+        var lineThrough = "line-through";
+        if(style != lineThrough){
+            element.css("text-decoration", lineThrough);
+        }else{
+            element.css("text-decoration", "");
+        }
+    });
 });
